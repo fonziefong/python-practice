e5d87be3eb51cadbe8c2c92ffab57252d5e18809
fonziefong <fonziefong@gmail.com>
2022-09-13 12:28:36 +0800

    Modified playground.py

--- playground.py
+++ playground.py
@@ -1,8 +1,34 @@
-from user import User
+from tree import TreeNode
 
-peter = User("Peter", "Get12345")
-print("Hashed password: {}".format(peter._hashed_password))
-print("Hashed password: {}".format(peter._hashed_password))
-peter.password = "Set12345"
-print("Hashed password: {}".format(peter._hashed_password))
-print("Salt: {}".format(peter.salt))
+print("Code: root = TreeNode(\"root\")")
+root = TreeNode("root")
+print("Code: child1 = TreeNode(\"child 1\")")
+child1 = TreeNode("child 1")
+print("Code: child2 = TreeNode(\"child 2\")")
+child2 = TreeNode("child 2")
+print("Code: child3 = TreeNode(\"child 2\")")
+child3 = TreeNode("child 3")
+
+print("Code: root.children = [child1, child2]")
+root.children = [child1, child2]
+
+print("Code: root.children")
+root.children
+print("Code: print(root.children)")
+print(root.children)
+
+print("Code: print(child3.children)")
+print(child3.children)
+print("Code: root.children = child3")
+root.children = child3
+
+print("Code: root.children")
+root.children
+print("Code: print(root.children)")
+print(root.children)
+
+
+print("Code: del root.children")
+del root.children
+print("Code: root.children")
+root.children
